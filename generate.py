bc9584ea76ed75dc0b12aeb5ea41ac022b3a4ede
jonathandaniels <jsdaniels97@gmail.com>
2023-03-08 13:45:27 -0500

    removed hidden .git files

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 # use pyrosim to generate a link
 import pyrosim.pyrosim as pyrosim
 
-# store info about world in world.sdf
+# # store info about world in world.sdf
 pyrosim.Start_SDF("box.sdf")
 pyrosim.Send_Cube(name="box", pos=[0, 0, 0.5], size=[1, 1, 1])
 
