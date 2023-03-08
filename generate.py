5cf567f90d3415ed41776846b39cae05cc65d2e9
jonathandaniels <jsdaniels97@gmail.com>
2023-03-08 13:33:05 -0500

    spacing for generate.py

--- generate.py
+++ generate.py
@@ -1,3 +1,9 @@
 # use pyrosim to generate a link
-# store world.sdf
-# read it in and simulate it in simulate.py
+import pyrosim.pyrosim as pyrosim
+
+# store info about world in world.sdf
+pyrosim.Start_SDF("box.sdf")
+pyrosim.Send_Cube(name="box", pos=[0, 0, 0.5], size=[1, 1, 1])
+
+# read it in and simulate it in simulate.py and close file
+pyrosim.End()
