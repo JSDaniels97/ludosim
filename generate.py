ce3ad64a9a96927a7eb5f0b4a1f66835272fcc4e
jonathandaniels <jsdaniels97@gmail.com>
2023-03-09 11:59:56 -0500

    fixed box placement on floor

--- generate.py
+++ generate.py
@@ -5,10 +5,10 @@ import pyrosim.pyrosim as pyrosim
 
 # # store info about world in world.sdf
 pyrosim.Start_SDF("box.sdf")
-length = 1
-width = 2
-height = 3
-pyrosim.Send_Cube(name="box", pos=[0, 0, 0.5], size=[length, width, height])
+[length, width, height] = [1, 2, 3]
+[x, y, z] = [0, 0, 1.5]
+
+pyrosim.Send_Cube(name="box", pos=[x, y, z], size=[length, width, height])
 
 # read it in and simulate it in simulate.py and close file
 pyrosim.End()
