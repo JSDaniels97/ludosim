c13209a46a37ff3a11d9491a234e71aa2cb8afc5
jonathandaniels <jsdaniels97@gmail.com>
2023-03-09 12:36:13 -0500

    tower of boxes

--- generate.py
+++ generate.py
@@ -8,10 +8,18 @@ pyrosim.Start_SDF("boxes.sdf")
 [length, width, height] = [1, 1, 1]
 [x, y, z] = [0, 0, 0.5]
 
-pyrosim.Send_Cube(name="box", pos=[x, y, z], size=[length, width, height])
-pyrosim.Send_Cube(name="box2", pos=[1, y, 1.5], size=[length, width, height])
+# individual cubes
+# pyrosim.Send_Cube(name="box", pos=[x, y, z], size=[length, width, height])
+# pyrosim.Send_Cube(name="box2", pos=[1, y, 1.5], size=[length, width, height])
+
+# tower of cubes
+for i in range(10):
+    pyrosim.Send_Cube(name="box", pos=[x, y, z+i],
+                      size=[length*(1-(i/10)), width*(1-(i/10)), height*(1-(i/10))])
 
 # read it in and simulate it in simulate.py and close file
 pyrosim.End()
 
 # NOTE: you must run this to generate the box.sdf file so it can be pulled into the simulation
+
+
