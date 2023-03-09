83c1007016048c86159115e8109fd3cb8a5373e4
jonathandaniels <jsdaniels97@gmail.com>
2023-03-09 10:38:51 -0500

    threw a box in the simulation

--- simulate.py
+++ simulate.py
@@ -1,7 +1,13 @@
 import time
-
 import pybullet as p
+
+# connect to physics server
 physicsClient = p.connect(p.GUI)
+
+# read in the world into the server
+p.loadSDF("box.sdf")
+
+# do the following within the world
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
